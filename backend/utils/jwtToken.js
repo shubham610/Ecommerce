4e62085764e4da890289d0850611b3346b237ea9
shubham bansal <shubh.6102001@gmail.com>
2023-09-06 15:40:56 +0530

    solved cookie issue for local host

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -6,14 +6,10 @@ const sendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
-    // sameSite: "none",
-    // secure: true,
-    sameSite: "lax",
-    secure: false,
-    domain: "http://localhost:3000",
+    sameSite: "none",
+    secure: true,
   };
   console.log(res.cookie("token", token));
-  res.cookie("token", token);
   res.status(statusCode).cookie(`token`, token, options).json({
     success: true,
     token,
